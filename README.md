5b408b6a41ea3e60f254370854776ef3c5cd3646
= <=>
2017-12-22 23:36:39 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-
+天池工业AI大赛-智能制造质量预测
+https://tianchi.aliyun.com/competition/introduction.htm?spm=5176.100068.5678.1.77569100X3QdIh&raceId=231633
---
+++ data_cleaner.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pandas as pd
+
+root_dir = os.path.split(os.path.abspath(__file__))[0].replace("\\", "/") + "/"
